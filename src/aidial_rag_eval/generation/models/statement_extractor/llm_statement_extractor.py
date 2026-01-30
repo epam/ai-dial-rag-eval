8d6a9ea790294127bb03a9f94eb85d154a6169fb
Efreet408 <34380256+Efreet408@users.noreply.github.com>
2026-01-30 10:51:13 -0500

    feat: сhanged error handling in the statement extractor (#61)

--- src/aidial_rag_eval/generation/models/statement_extractor/llm_statement_extractor.py
+++ src/aidial_rag_eval/generation/models/statement_extractor/llm_statement_extractor.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 
 from langchain_core.language_models import BaseChatModel
-from langchain_core.runnables import RunnableSerializable, chain
+from langchain_core.runnables import RunnablePassthrough, RunnableSerializable, chain
 
 from aidial_rag_eval.generation.models.lambdas import json_to_list
 from aidial_rag_eval.generation.models.statement_extractor.base_statement_extractor import (
@@ -16,31 +16,42 @@ from aidial_rag_eval.generation.utils.progress_bar import ProgressBarCallback
 
 @chain
 def list_to_statements(
-    statements_for_each_hypothesis: List[Dict[str, List[str]]],
+    llm_outputs_with_inputs: Dict,
 ) -> List[List[str]]:
     """
     Function is part of a chain that extracts segments from a list.
 
     Parameters
     -----------
-    statements_for_each_hypothesis : List[Dict[str, List[str]]]
-        The output list of dicts from the LLM with extracted statements.
+    llm_outputs_with_inputs : Dict
+        Passed inputs with output list of dicts from the LLM with extracted statements.
 
     Returns
     ------------
-    List[str]
+    List[List[str]]
         The extracted statements. if the LLM output is valid;
-        otherwise, an empty list is returned.
+        otherwise, return an original list of hypothesis_segments,
+        each hypothesis_segment is wrapped.
     """
     try:
+        statements_for_hypothesis_segments = llm_outputs_with_inputs[
+            "llm_output_statements"
+        ]
+        hypothesis_segments = llm_outputs_with_inputs["hypothesis_segments"]
+        assert len(hypothesis_segments) == len(statements_for_hypothesis_segments)
         return [
-            return_dict["statements"] for return_dict in statements_for_each_hypothesis
+            return_dict["statements"]
+            for return_dict in llm_outputs_with_inputs["llm_output_statements"]
         ]
     except (
         TypeError,
         KeyError,
+        AssertionError,
     ):
-        return []
+        return [
+            [hypothesis_segment]
+            for hypothesis_segment in llm_outputs_with_inputs["hypothesis_segments"]
+        ]
 
 
 @chain
@@ -48,8 +59,8 @@ def wrap_hypotheses(input_: Dict) -> Dict:
     assert type(input_) is dict
     return {
         "hypotheses": [
-            f"<hypothesis{index + 1}> {hypothesis} </hypothesis{index + 1}>"
-            for index, hypothesis in enumerate(input_["hypotheses"])
+            f"<hypothesis{index + 1}> {hypothesis_segment} </hypothesis{index + 1}>"
+            for index, hypothesis_segment in enumerate(input_["hypothesis_segments"])
         ],
     }
 
@@ -76,17 +87,19 @@ class LLMStatementExtractor(StatementExtractor):
     ):
 
         self._chain = (
-            wrap_hypotheses
-            | statement_prompt
-            | model
-            | json_to_list
+            RunnablePassthrough.assign(
+                llm_output_statements=wrap_hypotheses
+                | statement_prompt
+                | model
+                | json_to_list
+            )
             | list_to_statements
         )
         self.max_concurrency = max_concurrency
 
     def extract(
         self,
-        hypothesis_segments: List[List[HypothesisSegment]],
+        list_of_hypothesis_segments: List[List[HypothesisSegment]],
         show_progress_bar: bool,
     ) -> List[List[List[Statement]]]:
         """
@@ -95,7 +108,7 @@ class LLMStatementExtractor(StatementExtractor):
 
         Parameters
         -----------
-        hypothesis_segments : List[List[HypothesisSegment]]
+        list_of_hypothesis_segments : List[List[HypothesisSegment]]
             A list of hypothesis segments as a sources of statements.
 
         show_progress_bar : bool
@@ -107,13 +120,15 @@ class LLMStatementExtractor(StatementExtractor):
             Returns the statements for each hypothesis segment.
         """
 
-        with ProgressBarCallback(len(hypothesis_segments), show_progress_bar) as cb:
+        with ProgressBarCallback(
+            len(list_of_hypothesis_segments), show_progress_bar
+        ) as cb:
             returns = self._chain.batch(
                 [
                     {
-                        "hypotheses": hypotheses,
+                        "hypothesis_segments": hypothesis_segments,
                     }
-                    for hypotheses in hypothesis_segments
+                    for hypothesis_segments in list_of_hypothesis_segments
                 ],
                 config={"callbacks": [cb], "max_concurrency": self.max_concurrency},
             )
