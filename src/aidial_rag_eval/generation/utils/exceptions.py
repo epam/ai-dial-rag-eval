99407cda2baa9ae287f2e77cb2f8576c0ab38606
efreet408 <efreet408@gmail.com>
2026-03-15 14:14:17 -0500

    ErrorInfo.name -> ErrorInfo.error_repr

--- src/aidial_rag_eval/generation/utils/exceptions.py
+++ src/aidial_rag_eval/generation/utils/exceptions.py
@@ -5,6 +5,6 @@ from aidial_rag_eval.generation.types import ErrorInfo
 
 def make_error_info(exc: BaseException) -> ErrorInfo:
     return ErrorInfo(
-        name=type(exc).__name__,
+        error_repr=repr(exc),
         traceback="".join(traceback.format_exception(exc)),
     )
