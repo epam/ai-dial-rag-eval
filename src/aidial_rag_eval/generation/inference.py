798b0b71f49c81fd54d24f577d4dca6539360d49
efreet408 <efreet408@gmail.com>
2026-03-26 15:10:00 -0500

    simplify inference

--- src/aidial_rag_eval/generation/inference.py
+++ src/aidial_rag_eval/generation/inference.py
@@ -1,6 +1,5 @@
 import itertools
 import json
-import math
 from typing import Iterable, List, Optional, Tuple, TypeVar, Union, cast
 
 import numpy as np
@@ -529,11 +528,8 @@ def calculate_batch_inference(
             score.error.error_repr if score.error else None
             for _, score in grouped_data_item
         ]
-        mean_inference = (
-            math.nan
-            if any(math.isnan(inference) for inference in inferences)
-            else float(np.average(inferences, weights=weights))
-        )
+        # mean_inference = float(np.average(inferences, weights=weights))
+        mean_inference = float(np.mean(inferences))
         # fill Nones with 0.0
         min_possible_inference = float(
             np.average(np.nan_to_num(inferences, nan=0.0), weights=weights)
